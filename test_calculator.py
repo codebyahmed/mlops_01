16cfbd6192e0b619296707ba3ae23d9730d1181a
chughtaie <hafizchughtai09@gmail.com>
2023-09-05 09:44:44 -0700

    mul changed

--- test_calculator.py
+++ test_calculator.py
@@ -3,4 +3,4 @@ from calculator import  multiply
 def test_multiply():
     assert multiply(2,6) == 12
     assert multiply(2.5,-5) == -12.5
-    assert multiply(-3,6) == -18
+    assert multiply(3,6) == 18
